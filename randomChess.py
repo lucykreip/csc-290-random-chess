c62faf63b7f7a9290f7062794db65e109554def0
Mikayle Despaigne <158209632+mikayledesp@users.noreply.github.com>
2025-10-01 23:08:08 -0400

    took away isbotMove and just used board.turn since its alr flipped by board.push + added some print statements in their

--- randomChess.py
+++ randomChess.py
@@ -5,62 +5,59 @@ def main ():
     print("Welcome to Chess!")
     botColor = input("Should the computer player be black or white(b or w) ?")
     startingFEN = input("Starting FEN position? (hit ENTER for standard starting postion)")
-    board = None
+    # initializing board
     if (startingFEN == ""):
         board = chess.Board()
     else:
         board = chess.Board(startingFEN)
-    isOver = False
+
     botName = ""
     playerName = "" 
-    isbotMove = None
+    
     if (botColor == "w"):
        botColor = chess.WHITE
        botName = "Bot(as white)"
        playerName = "Player(as black)"
-    # isbotMove = board.turn(botColor)
-       isbotMove = True
-       board.turn = isBotMove
+       print(botName)
+       print(playerName)
     elif (botColor == "b"):
         botColor = chess.BLACK
         botName = "Bot(as black)"
         playerName = "Player(as white)"
-        isbotMove = False
-        board.turn = not isBotMove
+        print(botName)
+        print(playerName)
 
-    
+    print("Printing Initial Board......")
     print(board)
-
-    while (isOver == False) :
-        if (isbotMove == True):
+    print("-----------------")
+    while not board.is_game_over() :
+        # if its the bots turn 
+        if (board.turn == botColor):
             moveList =list(board.legal_moves)
-
             randIndex = random.randint(0,len(moveList)-1)
-
             randMove = moveList[randIndex]
-
-            print(randMove)
+            print("Bot moves to: ", randMove)
             board.push(randMove)
-            board.turn = not isbotMove
-            isBotMove = False
+            # isbotMove isn't needed since doing board.push alr flips the turns 
         else:
+            print(board)
+            move = input("Input Your Move: ")
+            board.push_uci(move) 
 
-            board.turn = isBotMove
-            isBotMove = True
+    print("Game Results:" , board.result())
+    board.reset()
 
-
-        isOver = True
 
 
             
     # TO DO : All we need to do is randomize from the list of legal moves for the bot  within the while loop !  
 
-    print(board)
-    print(botName)
-    print(playerName)
+    # print(board)
+    # print(botName)
+    # print(playerName)
     # board.turn = False
     # True == white and False == black 
-    print(board.turn)
+    # print(board.turn)
 
 
 
